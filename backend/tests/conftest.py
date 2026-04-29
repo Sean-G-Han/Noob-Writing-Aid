0fc43c840f29789f491361523772f814d6a2c8fe
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2026-04-29 18:04:53 +0800

    Fix a bug where "floating" tables may not be deleted during testing - change reset_db to reset all tables

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,6 +2,7 @@ import sqlite3
 import pytest
 from database.init_db import init_db
 
+#YH Notes: this gives a connection to in-mem SQLite DB
 @pytest.fixture(scope="session") # YH Notes: Basically like a wrapper for the test functions
 def conn(): #called whenever conn is used as an argument in a test function
     conn = sqlite3.connect(":memory:")
@@ -16,5 +17,14 @@ def conn(): #called whenever conn is used as an argument in a test function
 @pytest.fixture(autouse=True) # YH Notes: runs before each test function without needing to be called
 def reset_db(conn):
     yield # makes staments run AFTER
-    conn.execute("DELETE FROM novels")
+    conn.executescript(
+        "DELETE FROM chapter_to_character;\n"
+        "DELETE FROM character_adjectives;\n"
+        "DELETE FROM character_pronouns;\n"
+        "DELETE FROM character_alternative_names;\n"
+        "DELETE FROM chapters;\n"
+        "DELETE FROM characters;\n"
+        "DELETE FROM novels;\n"
+        "DELETE FROM sqlite_sequence;" # YH Notes: resets autoincrement IDs back to 1
+    )
     conn.commit()
